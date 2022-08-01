e63191d539e151c4a50a72834227a5c266ca8e9b
AthmanDodoe <jilloathman943@gmail.com>
2022-08-01 04:59:18 +0300

    a class Rectangle that defines a rectangle

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -1,51 +1,42 @@
 #!/usr/bin/python3
-""" Module 1-rectangle
-Defines a Rectangle class.2
+""" Provides a class 'Rectangle' to represent a rectangle
 """
 
 
-class Rectangle:
-    """ Rectangle class defined by width and height."""
-
+class Rectangle():
+    """ Definition of a class to represent a rectangle
+    """
     def __init__(self, width=0, height=0):
-        """ Initializes a Rectangle instance.
-
-        Args:
-            width: width of the rectangle
-            height: height of the rectangle
+        """ Instantiate a rectangle
         """
-        self.width = width
         self.height = height
+        self.width = width
 
     @property
     def width(self):
-        """ Retrieves the width of a rectangle instance."""
+        """ Get the width of a rectangle
+        """
         return self.__width
 
     @width.setter
     def width(self, value):
-        """ Sets the width of rectangle instance
-
-        Args:
-            value: value of the width, must be a positive integer
+        """ Set the width of a rectangle
         """
         if not isinstance(value, int):
             raise TypeError("width must be an integer")
-
         if value < 0:
             raise ValueError("width must be >= 0")
         self.__width = value
 
     @property
-    def height(self, value):
-        """ Retrieves the height of a Rectangle instance."""
+    def height(self):
+        """ Get the height of a rectangle
+        """
         return self.__height
 
     @height.setter
     def height(self, value):
-        """Sets the height of a Rectangle instance
-        Args:
-            value: value of the height, must be a positive integer
+        """ Set the height of a rectangle
         """
         if not isinstance(value, int):
             raise TypeError("height must be an integer")
